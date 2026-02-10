179f2d9f612f40c92000d8d2c48ab65e42a97006
Khánh Lê <l.x.k8042@gmail.com>
2026-02-10 22:32:47 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Bot, House, Pause, Users, X } from "lucide-react";
 
 const ROW_COUNT = 6;
 const COLUMN_COUNT = 7;
+const MAX_DEPTH = 3;
 
 const App = () => {
     const [board, setBoard] = useState(
@@ -15,6 +16,7 @@ const App = () => {
     const [gameOver, setGameOver] = useState(false);
     const [hoverTile, setHoverTile] = useState(null);
     const [draw, setDraw] = useState(false);
+    const [aiPlayer, setAiPlayer] = useState(false);
 
     const handleMouseEnter = (y) => {
         if (!gameOver) {
@@ -50,36 +52,45 @@ const App = () => {
         }
     };
 
-    const handleMove = (y) => {
+    const handleMove = (y, player) => {
         if (!gameOver) {
             y = parseInt(y);
-
             if (board[0][y] === 0) {
-                setBoard((row) => {
-                    const newBoard = row.map((col) => [...col]);
+                setBoard((prev) => {
+                    const newBoard = prev.map((col) => [...col]);
 
                     for (let i = ROW_COUNT - 1; i >= 0; i--) {
-                        for (let j = 0; j < COLUMN_COUNT; j++) {
-                            if (j === y && board[i][j] === 0) {
-                                turn === 0
-                                    ? (newBoard[i][j] = 1)
-                                    : (newBoard[i][j] = 2);
-                                checkWinMove(newBoard);
-                                setHoverTile({
-                                    x: i - 1 < 0 ? 0 : i - 1,
-                                    y: j,
-                                });
-                                return newBoard;
-                            }
+                        if (prev[i][y] === 0) {
+                            newBoard[i][y] = player;
+                            checkWinMove(newBoard, true) ||
+                                checkDrawMove(newBoard, true);
+                            setHoverTile({
+                                x: Math.max(0, i - 1),
+                                y,
+                            });
+                            return newBoard;
                         }
                     }
+                    return prev;
                 });
-                setTurn((prev) => (prev + 1) % 2);
             }
         }
     };
 
-    const checkWinMove = (newBoard) => {
+    const dropPiece = (checkBoard, y, checkTurn) => {
+        let newBoard = checkBoard.map((r) => [...r]);
+        if (newBoard[0][y] === 0) {
+            for (let i = ROW_COUNT - 1; i >= 0; i--) {
+                if (newBoard[i][y] === 0) {
+                    newBoard[i][y] = checkTurn;
+                    break;
+                }
+            }
+        }
+        return newBoard;
+    };
+
+    const checkWinMove = (newBoard, isPlace) => {
         for (let i = 0; i < ROW_COUNT; i++) {
             for (let j = 0; j < COLUMN_COUNT - 3; j++) {
                 if (
@@ -88,9 +99,11 @@ const App = () => {
                     newBoard[i][j] === newBoard[i][j + 3] &&
                     newBoard[i][j] !== 0
                 ) {
-                    setPause(true);
-                    setGameOver(true);
-                    return;
+                    if (isPlace) {
+                        setPause(true);
+                        setGameOver(true);
+                    }
+                    return true;
                 }
             }
         }
@@ -103,9 +116,11 @@ const App = () => {
                     newBoard[i][j] === newBoard[i + 3][j] &&
                     newBoard[i][j] !== 0
                 ) {
-                    setPause(true);
-                    setGameOver(true);
-                    return;
+                    if (isPlace) {
+                        setPause(true);
+                        setGameOver(true);
+                    }
+                    return true;
                 }
             }
         }
@@ -118,9 +133,11 @@ const App = () => {
                     newBoard[i][j] === newBoard[i + 3][j + 3] &&
                     newBoard[i][j] !== 0
                 ) {
-                    setPause(true);
-                    setGameOver(true);
-                    return;
+                    if (isPlace) {
+                        setPause(true);
+                        setGameOver(true);
+                    }
+                    return true;
                 }
             }
         }
@@ -133,22 +150,127 @@ const App = () => {
                     newBoard[i][j] === newBoard[i + 3][j - 3] &&
                     newBoard[i][j] !== 0
                 ) {
-                    setPause(true);
-                    setGameOver(true);
-                    return;
+                    if (isPlace) {
+                        setPause(true);
+                        setGameOver(true);
+                    }
+                    return true;
                 }
             }
         }
-
-        checkDrawMove(newBoard);
+        return false;
     };
 
-    const checkDrawMove = (newBoard) => {
+    const checkDrawMove = (newBoard, isPlace) => {
         if (!newBoard[0].includes(0)) {
-            setDraw(true);
-            setPause(true);
-            setGameOver(true);
+            if (isPlace) {
+                setDraw(true);
+                setPause(true);
+                setGameOver(true);
+            }
+            return true;
         }
+        return false;
+    };
+
+    const handleAiMove = () => {
+        let bestScore = -Infinity;
+        let bestMove = null;
+        const checkBoard = board.map((row) => [...row]);
+        console.log(checkBoard);
+
+        for (let col of possibleMoves(checkBoard)) {
+            const newBoard = dropPiece(checkBoard, col, 2);
+            const score = minimax(false, 0, -Infinity, Infinity, newBoard);
+
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = col;
+            }
+        }
+        handleMove(bestMove, 2);
+    };
+
+    const possibleMoves = (checkBoard) => {
+        let res = [];
+        for (let i = 0; i < COLUMN_COUNT; i++) {
+            if (checkBoard[0][i] === 0) {
+                res.push(i);
+            }
+        }
+        return res;
+    };
+
+    const minimax = (maximizing, depth, alpha, beta, predictBoard) => {
+        if (checkWinMove(predictBoard, false)) {
+            return maximizing ? -Infinity : Infinity;
+        } else if (checkDrawMove(predictBoard, false)) {
+            return 0;
+        } else if (depth === MAX_DEPTH) {
+            return scoreEvaluation(predictBoard, 2);
+        }
+
+        if (maximizing) {
+            let maxEval = -Infinity;
+            for (let col of possibleMoves(predictBoard)) {
+                const score = minimax(
+                    false,
+                    depth + 1,
+                    alpha,
+                    beta,
+                    dropPiece(predictBoard, col, 2),
+                );
+                maxEval = Math.max(maxEval, score);
+
+                alpha = Math.max(maxEval, alpha);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return maxEval;
+        } else {
+            let minEval = Infinity;
+            for (let col of possibleMoves(predictBoard)) {
+                const score = minimax(
+                    true,
+                    depth + 1,
+                    alpha,
+                    beta,
+                    dropPiece(predictBoard, col, 1),
+                );
+                minEval = Math.min(minEval, score);
+
+                beta = Math.min(minEval, beta);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return minEval;
+        }
+    };
+
+    const scoreEvaluation = (checkBoard, aiPlayer) => {
+        const evaluationBoard = [
+            [3, 4, 5, 7, 5, 4, 3],
+            [4, 6, 8, 10, 8, 6, 4],
+            [5, 7, 11, 13, 11, 7, 5],
+            [5, 7, 11, 13, 11, 7, 5],
+            [4, 6, 8, 10, 8, 6, 4],
+            [3, 4, 5, 7, 5, 4, 3],
+        ];
+
+        let aiScore = 0;
+        let playerScore = 0;
+        for (let i = 0; i < ROW_COUNT; i++) {
+            for (let j = 0; j < COLUMN_COUNT; j++) {
+                if (checkBoard[i][j] !== 0) {
+                    checkBoard[i][j] === aiPlayer
+                        ? (aiScore += evaluationBoard[i][j])
+                        : (playerScore += evaluationBoard[i][j]);
+                }
+            }
+        }
+        return aiScore - playerScore;
     };
 
     const handleNewGame = () => {
@@ -166,8 +288,10 @@ const App = () => {
     const menuNotification = () => {
         if (draw && gameOver) {
             return "Hòa";
+        } else if (gameOver && aiPlayer) {
+            return (turn + 1) % 2 === 0 ? "Người chơi thắng" : "AI thắng";
         } else if (gameOver) {
-            return (turn - 1) % 2 === 0
+            return (turn + 1) % 2 === 0
                 ? "Người chơi 1 thắng"
                 : "Người chơi 2 thắng";
         }
@@ -175,6 +299,13 @@ const App = () => {
     };
 
     useEffect(() => {
+        if (aiPlayer && !gameOver && turn % 2 === 1) {
+            const timer = setTimeout(() => {
+                handleAiMove();
+                setTurn((prev) => (prev + 1) % 2);
+            }, 300);
+            return () => clearTimeout(timer);
+        }
         return () => {};
     }, [board]);
 
@@ -196,12 +327,21 @@ const App = () => {
                             <House />
                         </button>
                         <button
-                            onClick={handleNewGame}
+                            onClick={() => {
+                                handleNewGame();
+                                setAiPlayer(false);
+                            }}
                             className="p-5 m-4 bg-green-500 rounded-lg cursor-pointer hover:bg-green-400 transition-colors duration-200"
                         >
                             <Users />
                         </button>
-                        <button className="p-5 m-4 bg-green-500 rounded-lg cursor-pointer hover:bg-green-400 transition-colors duration-200">
+                        <button
+                            onClick={() => {
+                                handleNewGame();
+                                setAiPlayer(true);
+                            }}
+                            className="p-5 m-4 bg-green-500 rounded-lg cursor-pointer hover:bg-green-400 transition-colors duration-200"
+                        >
                             <Bot />
                         </button>
                     </div>
@@ -219,7 +359,10 @@ const App = () => {
                     row.map((col, j) => {
                         return (
                             <div
-                                onClick={() => handleMove(j)}
+                                onClick={() => {
+                                    handleMove(j, turn === 0 ? 1 : 2);
+                                    setTurn((prev) => (prev + 1) % 2);
+                                }}
                                 onMouseEnter={() => handleMouseEnter(j)}
                                 onMouseLeave={() => handleMouseLeave(j)}
                                 key={`tile_${i}_${j}`}
